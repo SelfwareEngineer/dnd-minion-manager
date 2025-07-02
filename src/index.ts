da23591e350ddb0fbe2c0b37fb2c33bb7813b75e
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-02 00:28:40 -0400

    Set up dotenv
    - install dotenv
    - add loadEnv.ts to sidestep ESM's stupid import hoisting
    - convert hardcoded PORT to .env variable

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
+// ESM Thinks I'm too stupid to know what order I want to execute my own code in,
+// so configuring dotenv has to take place in a separate file. Juck FavaScript.
+import "./loadEnv.ts";
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
+console.log(process.env.PORT);
 
 app.listen(PORT, () => {
   console.log("Server up on port " + PORT);
 });
 
 app.get("/", (req, res) => {
-  res.send("is this thing on");
+  res.send("is this thing on? " + Math.random());
 });
