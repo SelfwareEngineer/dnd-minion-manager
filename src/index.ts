629bc2adef9e73efd4d4b2c676df2f8919e025ab
selfwareengineer <126501922+SelfwareEngineer@users.noreply.github.com>
2025-07-03 01:41:55 -0400

    Add example route handlers following tutorial; flesh out later

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,39 @@ app.listen(PORT, () => {
   console.log("Server up on port " + PORT);
 });
 
-app.get("/", (req, res) => {
-  res.send("is this thing on? " + Math.random());
 app.use(express.json());
 app.use(morgan("dev"));
 
-});
+// app.get("/api/v1/test", (req, res) => {
+//   res.json({
+//     message: "She lives!",
+//     randomNum: Math.random(),
+//   });
+// });
+//
+// app.post("/api/v1/weapons/:id", (req, res) => {
+//   console.log(req.body);
+//   res.json({ body: req.body, randNum: Math.random() });
+// });
+//
+// app.get("/api/v1/weapons", (req, res) => {
+//   res.json({
+//     data: {},
+//   });
+// });
+//
+// app.get("/api/v1/weapons/:id", (req, res) => {
+//   const params = req.params;
+//   console.log(params);
+//   res.json({
+//     data: { id: params.id, randNum: Math.random() },
+//   });
+// });
+//
+// app.put("/api/v1/weapons:id", (req, res) => {
+//   res.status(200);
+// });
+//
+// app.delete("/api/v1/weapons:id", (req, res) => {
+//   res.status(200);
+// });
