944a251080c8639729dfce31760c1e5827c914dd
Jay <126501922+SelfwareEngineer@users.noreply.github.com>
2025-10-22 20:36:08 -0400

    Add minimal CSS to conserve eyeballs

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,9 +4,11 @@ import viteLogo from "/vite.svg";
 
 function App() {
 	const [count, setCount] = useState(0);
+	const pageStyle =
+		"h-screen p-5 bg-gray-800 text-white flex flex-col items-center";
 
 	return (
-		<>
+		<div className={pageStyle}>
 			<div>
 				<a href="https://vite.dev" target="_blank">
 					<img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +33,7 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
-		</>
+		</div>
 	);
 }
 
